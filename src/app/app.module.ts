16c83b8f194812fc5bfb379a1fe4a11405cefefd
Tobias Redmann <tobias.redmann@cgi.com>
2022-01-03 15:57:36 +0100

    part3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,21 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ProductPageComponent } from './product-page/product-page.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
+import {ProductService} from './product.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
+    ProductPageComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
   ],
-  providers: [],
+  providers: [ProductService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/product.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {map} from 'rxjs/operators';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl = '../assets/album.json';
+
+  constructor(private _http: Http) {
+  }
+
+  getAlbum(id: number): any {
+    return this._http.get(this._albumUrl).pipe(map(value => value.json()));
+  }
+
+}
