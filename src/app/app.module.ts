6fc4d087d67c4ace279e23fd68134ddd09654444
Tobias Redmann <tobias.redmann@cgi.com>
2022-01-04 08:15:09 +0100

    part7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { ProductDescriptionComponent } from './product-description/product-descr
 import {ProductService} from './product.service';
 import { ProductTracklistingComponent } from './product-tracklisting/product-tracklisting.component';
 import { ProductListComponent } from './product-list/product-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: '', redirectTo: 'products', pathMatch: 'full'},
+  {path: 'products', component: ProductListComponent},
+  {path: 'product/:id', component: ProductPageComponent}
+];
 
 @NgModule({
   declarations: [
@@ -19,6 +26,7 @@ import { ProductListComponent } from './product-list/product-list.component';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     FormsModule,
     HttpModule,
