de138e589dd7b27fc0cf897fa2c7a2f08884c2ff
Tobias Redmann <tobias.redmann@cgi.com>
2022-01-04 07:17:47 +0100

    part3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {Http, Response} from '@angular/http';
-import {map} from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,8 +10,8 @@ export class ProductService {
   constructor(private _http: Http) {
   }
 
-  getAlbum(id: number): any {
-    return this._http.get(this._albumUrl).pipe(map(value => value.json()));
+  getAlbum(id: number) {
+    return this._http.get(this._albumUrl).map(response => response.json());
   }
 
 }
